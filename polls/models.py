c93febc6eadf94e5cd9eb26a1a1adeb7df599a1c
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2024-08-23 15:32:48 +0700

    Implemented tutorial part 2

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,34 @@
+import datetime
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
 
-class Questions(models.Model):
+class Question(models.Model):
+    '''
+    Questions model include question_text and published date
+    along with methods to check if the question was pulished recently (<= 1day)
+    '''
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        '''
+        Check if the question was pulished recently (<= 1day)
+        '''
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
+
+
 class Choice(models.Model):
-    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    '''
+    Choices for the polls questions
+    '''
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
